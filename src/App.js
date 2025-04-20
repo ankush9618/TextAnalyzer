8c43c89a8757fbe529536d863bd0d1d2690fea40
Ankush Kumar <ankushkumar9618@gmail.com>
2025-04-21 01:37:24 +0530

    modified the dark and light mode functionality to take min of screen height

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+
 import TextAnalyzer from './components/TextAnalyzer';
 
 function App() {
   return (
     <>
-    <TextAnalyzer/>
+      <TextAnalyzer />
     </>
   );
 }
--- src/components/TextAnalyzer.js
+++ src/components/TextAnalyzer.js
@@ -22,14 +22,14 @@ function TextAnalyzer() {
             if (i === 0 || text[i - 1] === " ") {
                 newText += text[i].toUpperCase();
             } else {
-                newText += text[i];
+                newText += text[i].toLowerCase();
             }
         }
         setText(newText);
     };
     const toCopyText = () => {
         navigator.clipboard.writeText(text);
-        alert("Text Copied Successfully..")
+        text === "" ? alert("Nothing to Copy") : alert("Text Copied Successfully..")
     };
 
     const toClearText = () => {
@@ -46,7 +46,7 @@ function TextAnalyzer() {
 
     return (
         <>
-            <div className="w-full h-auto place-content-center text-center pt-10 pb-10" style={mode === "white" ? { backgroundColor: "white", color: "black" } : { backgroundColor: "black", color: "white" }}>
+            <div className="w-full h-screen place-content-center text-center pt-10 pb-10" style={mode === "white" ? { backgroundColor: "white", color: "black" } : { backgroundColor: "black", color: "white" }}>
                 <Toggle onModeChange={onModeChange} mode={mode} />
                 <div className="mx-auto ">
                     <h1 className="text-2xl font-bold pt-6 pb-6">Text Analyzer</h1>
@@ -56,26 +56,26 @@ function TextAnalyzer() {
                         value={text}
                     ></textarea>
                     <div className="flex flex-wrap gap-4 justify-center pt-4 pb-4">
-                        <button className="bg-sky-900 p-2 rounded-xl font-bold text-white" onClick={toUppercase}>TO UPPER CASE</button>
-                        <button className="bg-sky-900 p-2 rounded-xl font-bold text-white" onClick={toLowerCase}>to lower case</button>
-                        <button className="bg-sky-900 p-2 rounded-xl font-bold text-white" onClick={toCamelCase}>To Camel Case</button>
-                        <button className="bg-sky-900 p-2 rounded-xl font-bold text-white" onClick={toCopyText}>Copy Text</button>
-                        <button className="bg-sky-900 p-2 rounded-xl font-bold text-white" onClick={toClearText}>Clear Text</button>
+                        <button className="bg-sky-900 p-2 rounded-xl font-bold text-white" disabled={text === ""} onClick={toUppercase}>TO UPPER CASE</button>
+                        <button className="bg-sky-900 p-2 rounded-xl font-bold text-white" disabled={text === ""} onClick={toLowerCase}>to lower case</button>
+                        <button className="bg-sky-900 p-2 rounded-xl font-bold text-white" disabled={text === ""} onClick={toCamelCase}>To Camel Case</button>
+                        <button className="bg-sky-900 p-2 rounded-xl font-bold text-white" disabled={text === ""} onClick={toCopyText}>Copy Text</button>
+                        <button className="bg-sky-900 p-2 rounded-xl font-bold text-white" disabled={text === ""} onClick={toClearText}>Clear Text</button>
                     </div>
                     <div>
                         <h1 className="text-2xl font-bold text-orange-400">
                             Your Text Summary
                         </h1>
                         <p className="text-xl p-2">
-                            Your Text contains {text.length} character,{" "}
-                            {text === "" ? 0 : text.split(" ").length} words and{" "}
+                            Your Text contains {text.length} characters,{" "}
+                            {text === "" ? 0 : text.split(/\s/).filter((ele) => { return ele.length !== 0 }).length} words and{" "}
                             {text === "" ? 0 : text.split(".").length - 1} sentenses.
                         </p>
-                        <p className="text-xl">It takes <span className="pr-1 pl-1 bg-yellow-300 font-semibold text-black">{text === "" ? 0 : (text.split(" ").length) * 0.008} minutes</span> to read.</p>
+                        <p className="text-xl">It takes <span className="pr-1 pl-1 bg-yellow-300 font-semibold text-black">{text === "" ? 0 : (text.split(/\s/).filter((ele) => { return ele.length !== 0 }).length) * 0.008} minutes</span> to read.</p>
                     </div>
                     <div className="pt-4 w-[80%] mx-auto">
                         <h1 className="text-3xl font-bold pl-1 md:text-left"><span className="text-4xl pl-1 pr-1" style={mode === "white" ? { color: "white", backgroundColor: "black" } : { color: "black", backgroundColor: "white" }}>P</span>review</h1>
-                        <h1 className="p-2 text-xl pt-2">{text === "" ? "Enter Some Text to Analyze..." : text}</h1>
+                        <h1 className="p-2 text-xl pt-2">{text === "" ? "Nothing to Preview...." : text}</h1>
                     </div>
                 </div>
             </div>
